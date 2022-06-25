563a19d4ae461fb77c54d1b6c4f977079507b5a2
aadiosap <aadiosap@unal.edu.co>
2022-06-25 16:12:42 -0500

    Update create_data_lake.py

--- src/data/create_data_lake.py
+++ src/data/create_data_lake.py
@@ -1,3 +1,7 @@
+from ntpath import join
+from pandas import concat
+
+
 def create_data_lake():
     """Cree el data lake con sus capas.
 
@@ -25,19 +29,18 @@ def create_data_lake():
 
     data_lake_dirs=["landing","raw","cleansed","business"]
     bussines_dirs=["reports","features","forecasts"]
-    reports_dirs=["figures"]
-
+    
     os.makedirs('data_lake', exist_ok=True)
 
     for dicts in data_lake_dirs:
-        new_path="data_lake/"&dicts
+        new_path="data_lake/"+str(dicts)
         os.makedirs(new_path,exist_ok=True)
 
     for dicts in bussines_dirs:
-        new_path="data_lake/business"&dicts
+        new_path="data_lake/business/"+str(dicts)
         os.makedirs(new_path,exist_ok=True)
     
-    os.makedirs('data_lake/reports/figures',exist_ok=True)
+    os.makedirs('data_lake/business/reports/figures',exist_ok=True)
 
     raise NotImplementedError("Implementar esta función")
 
