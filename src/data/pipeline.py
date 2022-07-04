24220a012b60afc7b2326639b826318f66767528
aadiosap <aadiosap@unal.edu.co>
2022-07-04 13:38:25 -0500

    main

--- src/data/pipeline.py
+++ src/data/pipeline.py
@@ -20,66 +20,58 @@ En luigi llame las funciones que ya creo.
 
 """
 import luigi
-from luigi import LocalTarget, Task, LocalTarget
+from luigi import Task, LocalTarget
+import os
+import pandas as pd
+from ingest_data import ingest_data
+from transform_data import transform_data
+from clean_data import clean_data
+from compute_monthly_prices import compute_monthly_prices
+from compute_daily_prices import compute_daily_prices
 
-class ingest_data_pipeline(Task):
+class Ingest_Transform_Clean_Data(Task):
     def output(self):
-        return LocalTarget('data_lake/landing/result.txt')
-    
-    def run(self):
-        import ingest_data
-        with self.output().open('w') as outfile:
-            ingest_data.ingest_data()
+        return LocalTarget("data_lake/cleansed/precios-horarios.csv")
 
-class transform_data_pipeline(Task):
+    def run(self):
+        ingest_data()
+        print("ingest_data-----------------OK")
+        transform_data()
+        print("transform_data----------OK")
+        clean_data()
+        print("clean_data-----------OK")
+
+class compute_daily_prices(Task):
+    def requires (self):
+        return Ingest_Transform_Clean_Data()
+
+    def output(self):
+        return LocalTarget('data_lake/business/precios-diarios.csv')
+
+    def run(self):
+        compute_daily_prices()
+        print("compute_daily_prices------------OK")
+
+class compute_monthly_prices(Task):
     def requires(self):
-        return ingest_data_pipeline()
-    
+        return Ingest_Transform_Clean_Data()
+
     def output(self):
-        return LocalTarget('data_lake/raw/result.txt')
+        return LocalTarget('data_lake/business/precios-mensuales.csv')
 
     def run(self):
-        import transform_data
-        with self.output().open('w') as outfile:
-            transform_data.transform_data()
+        compute_monthly_prices()
+        print("compute_monthly_prices-------------------OK")
 
-class clean_data_pipeline(Task):
+class Pipe_Run_All(Task):
+
     def requires(self):
-        return transform_data_pipeline()
-    
-    def output(self):
-        return LocalTarget('data_lake/cleansed/result.txt')
-
-    def run(self):
-        import clean_data
-        with self.output().open('w') as outfile:
-            clean_data.clean_data()
-
-class daily_reports_pipeline(Task):
-    def requires(self):
-        return clean_data_pipeline()
-    
-    def output(self):
-        return LocalTarget('data_lake/business/daily_prices.txt')
-
-    def run(self):
-        import compute_daily_prices
-        with self.output().open('w') as outfile:
-            compute_daily_prices.compute_daily_prices()        
-
-class monthly_reports_pipeline(Task):
-    def requires(self):
-        return daily_reports_pipeline()
-    
-    def output(self):
-        return LocalTarget('data_lake/business/monthly_prices.txt')
-
-    def run(self):
-        import compute_monthly_prices
-        with self.output().open('w') as outfile:
-            compute_monthly_prices.compute_monthly_prices() 
+        return [
+            compute_daily_prices(),
+            compute_monthly_prices(),
+        ]
 
 if __name__ == "__main__":
     import doctest
-    luigi.run(['monthly_reports_pipeline','--local-scheduler'])
+    luigi.run(['Pipe_Run_All','--local-scheduler'])
     doctest.testmod()
